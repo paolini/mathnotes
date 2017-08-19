1b9309c43c022ceab8adef15b04c5d5e7b72f092
Emanuele Paolini <emanuele.paolini@gmail.com>
2017-08-19 09:57:19 +0200

    server is able to load json dumps into dynamodb

--- notes-server/src/server.js
+++ notes-server/src/server.js
@@ -2,47 +2,96 @@ import fs from 'fs';
 import AWS from 'aws-sdk';
 import uuid from 'node-uuid';
 
+
 AWS.config.update({
   region: "eu-central-1"
 });
 
-function main() {
+function aws_putItems(items, done) {
   const dynamodb = new AWS.DynamoDB();
+  let count = 0;
 
-  const params = {
-    TableName: 'notes' /* required */
-  };
+  function callback(err, data) {
+    if (err) {
+      done(err, {count});
+      return;
+    }
+    if (count >= items.length) {
+      done(null, {count});
+      return;
+    }
+    if (count > 0) {
+      console.log("trasferred", count-1);
+    }
+    console.log("trasferring item...", count, items[count]);
+    dynamodb.putItem(items[count], callback);
+    count ++;
+  }
 
-  dynamodb.describeTable(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
+  callback();
 }
 
-function import_json_data(filename) {
+function import_json_data(filename, done) {
   const obj = JSON.parse(fs.readFileSync(filename, 'utf8'));
   const notes = [];
   const users = [];
   const usersocialauth = [];
+
   function extract_item(item) {
-    return { ...item.fields, id: item.pk};
+    const o = {id: item.pk.toString()};
+    for (const [key, value] of Object.entries(item.fields)) {
+      if (value) {
+        o[key] = value;
+      }
+    }
+    return o;
   }
+
+  function prepare_params(data, table) {
+    const Item = {};
+    for (let [key, value] of Object.entries(data)) {
+      if (typeof(value) == 'string') Item[key] = {'S': value};
+      else if (typeof(value) == 'boolean') Item[key] = {'BOOL': value};
+      else if (typeof(value) == 'number') Item[key] = {'N': value.toString()};
+      else {
+        throw 'unexpected key type ' + typeof(value);
+      }
+    }
+    return {TableName: table, Item};
+  }
+
+  const item_queue = [];
   for (const item of obj) {
     if (item.model === 'main.note') {
-      notes.push(extract_item(item));
+      const data = extract_item(item);
+      const params = prepare_params(data, 'notes');
+      item_queue.push(params);
     } else if (item.model === 'auth.user') {
-      users.push(extract_item(item));
+      const data = extract_item(item);
+      delete data.groups;
+      delete data.user_permissions;
+      item_queue.push(prepare_params(data, 'users'));
     } else if (item.model === 'default.usersocialauth') {
       const data = { ...extract_item(item)};
       data.extra_data = JSON.parse(data.extra_data);
-      usersocialauth.push(data);
     }
   }
-  return {notes, users, usersocialauth};
+
+  const dynamodb = new AWS.DynamoDB();
+
+  function aws_callback(err, data) {
+    AWS.putItem
+  }
+
+  aws_putItems(item_queue, done);
 }
 
 console.log(process.argv)
 if (process.argv[2] === 'import') {
-  const obj = import_json_data(process.argv[3]);
-  console.log('import obj', obj);
+  import_json_data(process.argv[3], (err, data) => {
+    if (err) {
+      console.log("ERROR!", err.stack);
+    }
+    console.log(data);
+  });
 }
